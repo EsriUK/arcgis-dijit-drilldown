5fa39792c73b2a977809529b9f2dc315ad1a17f9
pbradbury <pbradbury@esriuk.com>
2015-09-29 11:29:33 +0100

    Added extra tests

--- js/tests/integration_tests/drilldown_tests.js
+++ js/tests/integration_tests/drilldown_tests.js
@@ -40,7 +40,72 @@ describe('Drilldown tests: ' + helper.getBrowser(), function () {
             .pause(2000)
             .click(".esri-icon-search")
             .pause(10000)
-            //.click(".zoomTo")
+            .click(".zoomTo")
+            .pause(2000)
+            .getText("#searchWidget_more_results div.moreItem").then(function (text) {
+                console.log(text);
+            })
             .call(done);
     });
+    
+    it('should do an address search for Princes Street then select 12 Princes street from list', function (done) {
+        // Load drilldown test site
+        browser
+            .url("http://appsstage.esriuk.com/app/Drilldown-demo/13/wmt/view/416bf5a4f02f45158a0a893c73450878/index.html")
+
+            // Perform a valid search
+            .waitForExist("#searchWidget_input", 10000)
+            .pause(2000)
+            .setValue('#searchWidget_input', 'Princes Street')
+            .pause(2000)
+            .click(".esri-icon-search")
+            .pause(10000)
+            .click("#dijit_layout_ContentPane_6 span.drilldownResult")
+            .pause(10000)
+            .getText("#searchWidget_more_results div.moreItem").then(function(text) {
+                console.log(text);
+            })
+            .call(done);
+    });
+   
+    it('should do an address search for EH1 2AN from list then select 26 Rutland Street', function (done) {
+        // Load drilldown test site
+        browser
+            .url("http://appsstage.esriuk.com/app/Drilldown-demo/13/wmt/view/416bf5a4f02f45158a0a893c73450878/index.html")
+
+            // Perform a valid search
+            .waitForExist("#searchWidget_input", 10000)
+            .pause(2000)
+            .setValue('#searchWidget_input', 'EH1 2AN')
+            .pause(2000)
+            .click(".esri-icon-search")
+            .pause(10000)
+            .click("#dijit_layout_ContentPane_4 span.drilldownResult")
+            .pause(10000)
+            .getText("#searchWidget_more_results div.moreItem").then(function (text) {
+                console.log(text);
+            })
+            .call(done);
+    });
+    it('should do an address search for Princes Street then select top heading and Unit 2', function (done) {
+        // Load drilldown test site
+        browser
+            .url("http://appsstage.esriuk.com/app/Drilldown-demo/13/wmt/view/416bf5a4f02f45158a0a893c73450878/index.html")
+
+            // Perform a valid search
+            .waitForExist("#searchWidget_input", 10000)
+            .pause(2000)
+            .setValue('#searchWidget_input', 'Princes Street')
+            .pause(2000)
+            .click(".esri-icon-search")
+            .pause(10000)
+            .click("#dijit_TitlePane_1")
+            .click("#dijit_layout_ContentPane_1 span.drilldownResult")
+            .pause(10000)
+            .getText("#searchWidget_more_results div.moreItem").then(function (text) {
+                console.log(text);
+            })
+            .call(done);
+    });
+
 });
